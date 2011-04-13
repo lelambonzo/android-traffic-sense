7df8d927d46b33e91a8ee7077469af0c965cb408
eminem.virus2010@gmail.com <eminem.virus2010@gmail.com@86c68d29-1120-9f6e-e991-ced5053861db>
2011-04-13 18:19:41 +0000

    Initial Hello World commit

---
+++ trunk/android-client/src/com/traffic/sense/TrafficDaemon.java
@@ -0,0 +1,21 @@
+package com.traffic.sense;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * @author Amr Tj. Wallas
+ */
+public class TrafficDaemon extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+	super.onCreate(savedInstanceState);
+	TextView tv = new TextView(this);
+	tv.setText("Hello, Android");
+	setContentView(tv);
+    }
+}
