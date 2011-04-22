7d0744eab6b755a43e92dd41859c1124d09a13c2
eminem.virus2010 <eminem.virus2010@gmail.com>
2011-04-22 16:22:50 +0000

    - Added more platform dependent files to SVN:ignore - Created initial Hello World Controllers - Enabled crud and secure play modules - Created the initial layout for the database (models)

---
+++ web-server/app/controllers/Statistics.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package controllers;
+
+import models.Update;
+
+/**
+ * @author Amr Tj. Wallas
+ * 
+ */
+@CRUD.For(Update.class)
+public class Statistics extends CRUD
+{
+
+}
---
+++ web-server/app/models/Update.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import play.db.jpa.Model;
+
+/**
+ * @author Amr Tj. Wallas
+ * 
+ */
+@Entity
+public class Update extends Model
+{
+    @ManyToOne
+    public Client client;
+    public double longitude;
+    public double latitude;
+    public double altitude;
+    public float bearing;
+    public float accuracy;
+    public float speed;
+    public Date date;
+
+    public Update(Client client, double longitude, double latitude,
+	    double altitude, float bearing, float accuracy, float speed)
+    {
+	this.client = client;
+	this.longitude = longitude;
+	this.latitude = latitude;
+	this.altitude = altitude;
+	this.bearing = bearing;
+	this.accuracy = accuracy;
+	this.speed = speed;
+	this.date = new Date();
+    }
+}
