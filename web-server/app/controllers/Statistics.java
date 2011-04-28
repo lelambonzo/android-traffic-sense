5bff77951cbe0a45fa7efa95cc92963bca1c6ea1
eminem.virus2010 <eminem.virus2010@gmail.com>
2011-04-28 20:53:11 +0000

    Updated Documentation.

--- web-server/app/controllers/Statistics.java
+++ web-server/app/controllers/Statistics.java
@@ -15,12 +15,27 @@ import models.Update;
 @CRUD.For(Update.class)
 public class Statistics extends CRUD
 {
+    /**
+     * renders the map page and passes to it the list of updates in the database
+     */
     public static void map()
     {
 	List<Update> updates = Update.findAll();
 	render(updates);
     }
 
+    /**
+     * Adds new Updates into the database given an HTTP post with the params
+     * 
+     * @param device_id
+     * @param longitude
+     * @param latitude
+     * @param altitude
+     * @param bearing
+     * @param accuracy
+     * @param speed
+     * @return <code>True</code> If the client is a new client to the system
+     */
     public static boolean newInfo(long device_id, double longitude,
 	    double latitude, double altitude, float bearing, float accuracy,
 	    float speed)
