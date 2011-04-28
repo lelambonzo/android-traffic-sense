b3cd3356b9b5598a8df6f88af680ce7fc0a9243c
eminem.virus2010 <eminem.virus2010@gmail.com>
2011-04-28 19:35:44 +0000

    - Created new method in the statistics controller to add new Updates. - Created a new method in Client model to add Updates related to each client.

--- web-server/app/models/Client.java
+++ web-server/app/models/Client.java
@@ -21,13 +21,21 @@ import play.db.jpa.GenericModel;
 public class Client extends GenericModel
 {
     @Id
-    public int device_id;
+    public long device_id;
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
     public List<Update> updates;
 
-    public Client(int cellId)
+    public Client(long device_id)
     {
-	this.device_id = cellId;
+	this.device_id = device_id;
 	this.updates = new ArrayList<Update>();
+    }
+
+    public void addUpdate(double longitude, double latitude, double altitude,
+	    float bearing, float accuracy, float speed)
+    {
+	this.updates.add(new Update(this, longitude, latitude, altitude,
+		bearing, accuracy, speed));
+	this.save();
     }
 }
