fd110cf7d78563e98971fa9e10b30f793145b2f6
eminem.virus2010@gmail.com <eminem.virus2010@gmail.com@86c68d29-1120-9f6e-e991-ced5053861db>
2011-04-28 19:35:44 +0000

    - Created new method in the statistics controller to add new Updates. - Created a new method in Client model to add Updates related to each client.

--- trunk/web-server/app/controllers/Statistics.java
+++ trunk/web-server/app/controllers/Statistics.java
@@ -5,6 +5,7 @@ package controllers;
 
 import java.util.List;
 
+import models.Client;
 import models.Update;
 
 /**
@@ -18,5 +19,20 @@ public class Statistics extends CRUD
     {
 	List<Update> updates = Update.findAll();
 	render(updates);
+    }
+
+    public static boolean newInfo(long device_id, double longitude,
+	    double latitude, double altitude, float bearing, float accuracy,
+	    float speed)
+    {
+	boolean neu = false;
+	Client c = Client.findById(device_id);
+	if (c == null)
+	{
+	    c = new Client(device_id).save();
+	    neu = true;
+	}
+	c.addUpdate(longitude, latitude, altitude, bearing, accuracy, speed);
+	return neu;
     }
 }
