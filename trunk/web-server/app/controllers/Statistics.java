2b1606b4e59a59fb88a42952b61d70a2a8f9ea63
eminem.virus2010@gmail.com <eminem.virus2010@gmail.com@86c68d29-1120-9f6e-e991-ced5053861db>
2011-05-07 03:50:15 +0000

    implemented advanced server filtering algorithm for the incoming GPS Coordinates. FINALLY !!

--- trunk/web-server/app/controllers/Statistics.java
+++ trunk/web-server/app/controllers/Statistics.java
@@ -3,6 +3,7 @@
  */
 package controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import models.Client;
@@ -15,6 +16,23 @@ import models.Update;
 @CRUD.For(Update.class)
 public class Statistics extends CRUD
 {
+    /**
+     * The value of one latitude degree in meters.
+     */
+    public final static double LATITUDE_DEG_IN_M = 111318.8449;
+    /**
+     * The value of one latitude degree per 1m distance.
+     */
+    public final static double ONE_M_LATITUDE_IN_DEG = 1.0 / LATITUDE_DEG_IN_M;
+    /**
+     * The value of PI according to google.
+     */
+    public final static double PI = 3.14159265;
+    /**
+     * The value of the radius of the earth in meters.
+     */
+    public final static double EARTH_RADIUS = 6378100;
+
     /**
      * renders the map page and passes to it the list of updates in the database
      */
@@ -35,21 +53,91 @@ public class Statistics extends CRUD
      * @param accuracy
      * @param speed
      * @param date
-     * @return <code>True</code> If the client is a new client to the system
+     * @return <code>True</code> If the update is not redundant.
      */
     public static boolean newInfo(long device_id, double longitude,
 	    double latitude, double altitude, float bearing, float accuracy,
 	    float speed, long date)
     {
-	boolean neu = false;
 	Client c = Client.findById(device_id);
 	if (c == null)
-	{
 	    c = new Client(device_id).save();
-	    neu = true;
-	}
-	c.addUpdate(longitude, latitude, altitude, bearing, accuracy, speed,
+
+	boolean redundant = isRedundant(c, longitude, latitude, bearing, speed,
 		date);
-	return neu;
+
+	if (!redundant)
+	    c.addUpdate(longitude, latitude, altitude, bearing, accuracy,
+		    speed, date);
+	return !redundant;
+    }
+
+    /**
+     * Checks whether a given update is redundant or not according to the given
+     * parameters.
+     * 
+     * @param c
+     * @param longitude
+     * @param latitude
+     * @param bearing
+     * @param speed
+     * @param date
+     * @return <code>True</code> if the update is redundant.
+     */
+    public static boolean isRedundant(Client c, double longitude,
+	    double latitude, float bearing, float speed, long date)
+    {
+	// TODO Does this client ACTUALLY represents a vehicle?
+	// TODO is it a two way road?
+	List<Update> updates = Update
+		.find("date >= ? AND date <= ? AND speed >= ? AND speed <= ? AND client.device_id != ? ORDER BY date DESC",
+			new Date(date - 10000), new Date(date + 10000),
+			speed - 2.77777778f, speed + 2.77777778f, c.device_id)
+		.fetch();
+	if (updates.isEmpty())
+	    return false;
+	for (Update u : updates)
+	{
+	    if (getDistance(u.longitude, u.latitude, longitude, latitude) <= 1.4)
+		if (Math.abs(bearing - u.bearing) <= 90)
+		    return true;
+	}
+	return false;
+    }
+
+    /**
+     * Gets Meters per 1 Degree of Longitude at a given latitude.
+     * 
+     * @param latitude
+     *            latitude in decimal degrees.
+     * @return double: Meters per 1 Degree of Longitude at a given latitude.
+     */
+    public static double getLongDegInMeters(double latitude)
+    {
+	return (LATITUDE_DEG_IN_M * Math.cos(latitude));
+	// return (1.0 / longDegInMeters);
+    }
+
+    /**
+     * Gets the distance in meters between two geographic points.
+     * 
+     * @param long1
+     *            longitude of point1 in decimal degrees.
+     * @param lat1
+     *            latitude of point1 in decimal degrees.
+     * @param long2
+     *            longitude of point2 in decimal degrees.
+     * @param lat2
+     *            latitude of point2 in decimal degrees.
+     * @return double: The distance between the two points in meters.
+     */
+    public static double getDistance(double long1, double lat1, double long2,
+	    double lat2)
+    {
+	double la1 = lat1 * LATITUDE_DEG_IN_M;
+	double la2 = lat2 * LATITUDE_DEG_IN_M;
+	double lo1 = long1 * getLongDegInMeters(lat1);
+	double lo2 = long2 * getLongDegInMeters(lat2);
+	return Math.sqrt((Math.pow(lo2 - lo1, 2)) + (Math.pow(la2 - la1, 2)));
     }
 }
