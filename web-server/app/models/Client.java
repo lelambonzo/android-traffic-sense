74715cec14d1feed356140c139f5b78ec8e7907a
eminem.virus2010 <eminem.virus2010@gmail.com>
2011-05-13 16:10:12 +0000

    - Added a new attribute lastUpdate which shows when the client has last sent an update to the server. - Update the method addUpdate such that it updates the lastUpdate attribute accordingly.

--- web-server/app/models/Client.java
+++ web-server/app/models/Client.java
@@ -4,6 +4,7 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -24,6 +25,7 @@ public class Client extends GenericModel
     public long device_id;
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
     public List<Update> updates;
+    public Date lastUpdate = null;
 
     public Client(long device_id)
     {
@@ -36,6 +38,10 @@ public class Client extends GenericModel
     {
 	this.updates.add(new Update(this, longitude, latitude, altitude,
 		bearing, accuracy, speed, date));
+	if(this.lastUpdate != null)
+	    this.lastUpdate = (date > this.lastUpdate.getTime())? new Date(date) : lastUpdate;
+	else
+	    this.lastUpdate = new Date(date);
 	this.save();
     }
 }
